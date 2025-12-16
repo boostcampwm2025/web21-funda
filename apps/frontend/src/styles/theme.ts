61a660cb290aef074668ec24d3e467c3a354210d
chaen <cw06155@gmail.com>
2025-12-16 17:15:09 +0900

    feat: 스타일 변수 추가 및 color -> token 이름 변경 related to #15

--- apps/frontend/src/styles/theme.ts
+++ apps/frontend/src/styles/theme.ts
@@ -1,12 +1,5 @@
-import { colors } from './color';
+import { borderRadius, colors } from './token';
 import { typography } from './typography';
-
-export const borderRadius = {
-  small: '8px',
-  medium: '16px',
-  large: '24px',
-  xlarge: '40px',
-} as const;
 
 export const lightTheme = {
   colors: colors.light,
